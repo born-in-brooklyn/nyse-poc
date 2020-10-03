9f3f4fc15cf2be07a2a6a8cb30ac9885d7450909
Mordecai Zibkoff <Mordecai_Zibkoff@ultimatesoftware.com>
2020-10-03 19:37:05 -0400

    fixed issue needing retries

--- test/specs/stocklist.js
+++ test/specs/stocklist.js
@@ -1,35 +1,40 @@
 describe('Stock directory page', () => {
-    beforeEach(async () => {
-        await browser.url('https://www.nyse.com/listings_directory/stock');
+
+    links = () => {
+        var linksSelector ="//tr/td[1]/a";
+        browser.waitUntil(() => {
+            var retval = $$(linksSelector);
+            return retval.length > 0;
+        });
+        return $$(linksSelector);
+    };
+
+    beforeEach(() => {
+        browser.url('https://www.nyse.com/listings_directory/stock');
     });
 
-    it('must display Symbol and Name for the corresponding company', async () => {
-        let symbolHeader = await $("//tr/th[1][text()='Symbol']");
-        let nameHeader = await $("//tr/th[2][text()='Name']");
+    it('must display Symbol and Name for the corresponding company', () => {
+        let symbolHeader = $("//tr/th[1][text()='Symbol']");
+        let nameHeader = $("//tr/th[2][text()='Name']");
         expect(symbolHeader).toBeDisplayed();
         expect(nameHeader).toBeDisplayed();
     });
 
 
-    it('must display data sorted by Symbol (ascending)', async () => {
-        let elements = await $$('//tr/td[1]/a');
+    it('must display data sorted by Symbol (ascending)', () => {
+        let elements = links();
         let actual = elements.map((v,i) => v.getText());
         let expected = [...actual].sort();
         expect(actual).toEqual(expected);
     });
 
-
-    it(' must display 10 records per page', async () => {
-        let lastRow = await $('//tr[10]'); 
-        expect(lastRow).toBeDisplayed();
-        
-        let actual = await await $$('//tr/td[1]/a');
+    it('must display 10 records per page', () => {
+        let actual = links();
         expect(actual.length).toEqual(10);
     });
 
-    it(' must provide a pager', async () => {
-        let pager = await $("//ul[@class='pagination']");
+    it('must provide a pager', () => {
+        let pager = $("//ul[@class='pagination']");
         expect(pager).toBeDisplayed();
     });
-
 });
